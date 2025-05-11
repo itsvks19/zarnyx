45f5dd5f2a4a5169201f5774ce70870147e4da3f
Vivek <itsvks19@gmail.com>
2025-05-11 15:55:53 +0530

    new parser and operator precedence

--- crates/zarnyx-cli/src/main.rs
+++ crates/zarnyx-cli/src/main.rs
@@ -1,12 +1,11 @@
 use std::io::{self, Write};
+use zarnyx::parser::Parser;
 
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
     let mut stdout = io::stdout();
-    let mut stderr = io::stderr();
 
     let mut input = String::new();
-    let mut env = zarnyx::Env::default();
 
     loop {
         write!(stdout, "→ ")?;
@@ -14,26 +13,9 @@ fn main() -> io::Result<()> {
 
         stdin.read_line(&mut input)?;
 
-        match run(input.trim(), &mut env) {
-            Ok(Some(val)) => writeln!(stdout, "{}", val)?,
-            Ok(None) => {}
-            Err(msg) => writeln!(stderr, "{}", msg)?,
-        }
+        let parse = Parser::new(&input).parse();
+        println!("{}", parse.debug_tree());
 
         input.clear();
-    }
-}
-
-fn run(input: &str, env: &mut zarnyx::Env) -> Result<Option<zarnyx::Val>, String> {
-    let parse = zarnyx::parse(input).map_err(|msg| format!("Parse error: {}", msg))?;
-
-    let evaluated = parse
-        .eval(env)
-        .map_err(|msg| format!("Evaluation error: {}", msg))?;
-
-    if evaluated == zarnyx::Val::Unit {
-        Ok(None)
-    } else {
-        Ok(Some(evaluated))
     }
 }
--- crates/zarnyx/src/lib.rs
+++ crates/zarnyx/src/lib.rs
@@ -1,31 +1,12 @@
-#[allow(dead_code)]
-mod binding_def;
-mod expr;
-mod func_def;
-mod stmt;
-mod val;
-mod env;
-mod utils;
+pub mod parser;
 
-pub use env::Env;
-pub use val::Val;
+mod lexer;
+mod syntax;
 
-#[derive(Debug)]
-pub struct Parse(stmt::Stmt);
-
-impl Parse {
-    pub fn eval(&self, env: &mut Env) -> Result<Val, String> {
-        self.0.eval(env)
-    }
-}
-
-pub fn parse(s: &str) -> Result<Parse, String> {
-    let (s, stmt) = stmt::Stmt::new(s)?;
-
-    if s.is_empty() {
-        Ok(Parse(stmt))
-    } else {
-        dbg!(s);
-        Err("input was not consumed fully by parser".to_string())
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
     }
 }
